3f4e91abadc752c88329ec3e2c9a92b6b9660ca0
tyleralbee <tyleralbee25@gmail.com>
2024-02-12 14:00:18 -0800

    fun postinstall

--- postinstall.js
+++ postinstall.js
@@ -1,9 +1,39 @@
-console.log(`
+import chalk from "chalk";
+
+const log = console.log;
+
+const brandColors = {
+    purple: "#be44c2",
+    lightpurple: "#e5b4e7",
+    blue: "#019acc",
+    lightblue: "#67c2e0",
+    green: "#82d900",
+    lightgreen: "#b4e866",
+    yellow: "#ffc70d",
+    lightyellow: "#ffdd6e",
+    orange: "#ff571f",
+    lightorange: "#ff9a79",
+};
+
+// Log brand message
+log(
+    chalk.hex(brandColors.purple).bold("@") +
+        chalk.hex(brandColors.lightpurple).bold("c") +
+        chalk.hex(brandColors.blue).bold("a") +
+        chalk.hex(brandColors.lightblue).bold("b") +
+        chalk.hex(brandColors.green).bold("i") +
+        chalk.hex(brandColors.lightgreen).bold("n") +
+        chalk.hex(brandColors.yellow).bold("l") +
+        chalk.hex(brandColors.lightyellow).bold("l") +
+        chalk.hex(brandColors.orange).bold("c") +
+        " " +
+        chalk.bold("prettier config")
+);
+
+log(`
   To use @cabinllc/prettier-config, add the following script to your package.json:
 
   "scripts": {
     "format": "prettier --write \\"**/*.{js,jsx,ts,tsx}\\""
   }
-
-  Thank you for installing @cabinllc/prettier-config!
 `);
