22e7f6ea2a2667d3fcee4726f9156b51f5347700
Tyler Albee <tyleralbee25@gmail.com>
2025-01-03 16:05:31 -0800

    Update index.js

--- index.js
+++ index.js
@@ -1,25 +1,38 @@
-/**
- * @type {import('prettier').Config & import("@ianvs/prettier-plugin-sort-imports").PluginConfig}
- */
-const config = {
-    tabWidth: 4,
-    trailingComma: "es5",
-    plugins: ["@ianvs/prettier-plugin-sort-imports"],
-    importOrder: [
+{
+    "$schema": "https://json.schemastore.org/prettierrc",
+    "tabWidth": 4,
+    "trailingComma": "es5",
+    "plugins": [
+        "@ianvs/prettier-plugin-sort-imports",
+        "prettier-plugin-packagejson",
+        "prettier-plugin-jsdoc"
+    ],
+    "importOrder": [
         ".css$",
         "",
+        "<TYPES>^(node:)",
         "<TYPES>",
         "<TYPES>^[.]",
         "",
         "^react$",
+        "<BUILTIN_MODULES>",
         "<THIRD_PARTY_MODULES>",
         "",
+        "@campfire/",
         "^~/",
+        "^@/",
         "^[.][.]/",
-        "^[.]/",
+        "^[.]/"
     ],
-    importOrderParserPlugins: ["typescript", "jsx"],
-    importOrderTypeScriptVersion: "5.3.0",
-};
-
-export default config;
+    "importOrderParserPlugins": ["typescript", "jsx"],
+    "importOrderTypeScriptVersion": "5.6.2",
+    "tsdoc": true,
+    "overrides": [
+        {
+            "files": "*.jsonc",
+            "options": {
+                "trailingComma": "none"
+            }
+        }
+    ]
+}
